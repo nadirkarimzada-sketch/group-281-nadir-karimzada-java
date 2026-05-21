9e2a8e916fbb07c58220d5812ed1070ef4339a8b
nadirkarimzada-sketch <nadir.karimzada@gmail.com>
2026-05-21 21:55:56 +0400

    dfssdf

---
+++ src/classwork_6/Runner.java
@@ -0,0 +1,36 @@
+package classwork_6;
+
+public class Runner {
+	public class Main {
+	    public static void main(String[] args) {
+	        for(int i = 1; i <= 10; i++) {
+	            System.out.println(i);
+	        }
+	    }
+	}
+	//2
+	public static void main(String[] args) {
+        int i = 1;
+        int sum = 0;
+
+        while(i <= 100) {
+            sum += i;
+            i++;
+        }
+
+        System.out.println(sum);
+   
+	
+	//
+ 
+
+        for(int i = 1; i <= 9; i++) {
+            for(int j = 1; j <= 9; j++) {
+                System.out.print(i * j + "\t");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
