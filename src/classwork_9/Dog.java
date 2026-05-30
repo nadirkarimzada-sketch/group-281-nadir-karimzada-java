844af482ffaf34d26312e6cb602aed59f180dac4
nadirkarimzada-sketch <nadir.karimzada@gmail.com>
2026-05-30 21:53:46 +0400

    classwork_9

---
+++ src/classwork_9/Dog.java
@@ -0,0 +1,24 @@
+package classwork_9;
+
+
+class Dog {
+//    String name;
+//    int age;
+//
+//    void bark() {
+//        System.out.println(name + ": Гав!");
+//    }
+////   
+//	  String name;
+//	    int age;
+//
+//	    Dog(String name, int age) {
+//	        this.name = name;
+//	        this.age = age;
+//	    }
+//
+//	    void bark() {
+//	        System.out.println(name + ": Гав!");
+//	    }
+
+    }
